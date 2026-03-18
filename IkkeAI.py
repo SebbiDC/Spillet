b61a80bebc5e37e52f4e9ef538248e2e6f032edb
SebbiDC <sebastina.s.hersvik@gmail.com>
2026-03-18 10:08:09 +0100

    gdws

--- IkkeAI.py
+++ IkkeAI.py
@@ -37,6 +37,59 @@ bullets = []  # Each bullet is [x, y, vx, vy]
 last_shot_time = 0
 fire_rate = 300  # Milliseconds between shots
 
+# Map
+TILE_SIZE = 50
+PLATFORM_HEIGHT = TILE_SIZE // 2  # Platforms are half the size of normal tiles
+map_layout = [
+    "11111111111111111111111111111111111111111111111111111111111111111111",
+    "10000000000010000000000000000000000000000000000000000000000000000001",
+    "10000000000010000000000000000000000000000000000000000000000000000001",
+    "10020000000010000000000000000000000000000000000000000000000000000001",
+    "10000000200010000000000000000000000000000000000000000000000000000001",
+    "10000020000010000000000000000000000000000000000000000000000000000001",
+    "10111111111010000000000000000000000000000000000000000000000000000001",
+    "10000000000010000000000000000000000000000000000000000000000000000001",
+    "10000000000010000000000000000000000000000000000000000000000000000001",
+    "10000000000010000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "10000000000000000000000000000000000000000000000000000000000000000001",
+    "11111111111111111111111111111111111111111111111111111111111111111111",
+]
+
+# 0 = air, 1 = solid wall/floor, 2 = platform (half height)
+def draw_map(surface):
+    for row_index, row in enumerate(map_layout):
+        for col_index, tile in enumerate(row):
+            x = col_index * TILE_SIZE
+            y = row_index * TILE_SIZE
+            if tile == "1":
+                pygame.draw.rect(surface, WHITE, (x, y, TILE_SIZE, TILE_SIZE))
+            elif tile == "2":
+                # Draw platform at the bottom half of the tile slot
+                pygame.draw.rect(surface, BLUE, (x, y + PLATFORM_HEIGHT, TILE_SIZE, PLATFORM_HEIGHT))
+
+def get_wall_rects():
+    walls = []
+    for row_index, row in enumerate(map_layout):
+        for col_index, tile in enumerate(row):
+            x = col_index * TILE_SIZE
+            y = row_index * TILE_SIZE
+            if tile == "1":
+                walls.append(("solid", pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)))
+            elif tile == "2":
+                # Collision rect sits at the bottom half of the tile slot
+                walls.append(("platform", pygame.Rect(x, y + PLATFORM_HEIGHT, TILE_SIZE, PLATFORM_HEIGHT)))
+    return walls
+
 
 
 # Game loop
@@ -69,7 +122,40 @@ while True:
         square_y = ground_y - square_size
         y_velocity = 0
         on_ground = True
-        
+
+    # Collision with map walls
+    on_ground_this_frame = False
+    player_rect = pygame.Rect(square_x, square_y, square_size, square_size)
+    for tile_type, wall in get_wall_rects():
+        if player_rect.colliderect(wall):
+            overlap_left = player_rect.right - wall.left
+            overlap_right = wall.right - player_rect.left
+            overlap_top = player_rect.bottom - wall.top
+            overlap_bottom = wall.bottom - player_rect.top
+            min_overlap = min(overlap_left, overlap_right, overlap_top, overlap_bottom)
+            if tile_type == "platform":
+                # Platforms only block from above, and only when falling
+                if min_overlap == overlap_top and y_velocity >= 0:
+                    square_y = wall.top - square_size
+                    y_velocity = 0
+                    on_ground_this_frame = True
+            else:
+                # Solid tiles block from all sides
+                if min_overlap == overlap_top:
+                    square_y = wall.top - square_size
+                    y_velocity = 0
+                    on_ground_this_frame = True
+                elif min_overlap == overlap_bottom:
+                    square_y = wall.bottom
+                    y_velocity = 0
+                elif min_overlap == overlap_left:
+                    square_x = wall.left - square_size
+                elif min_overlap == overlap_right:
+                    square_x = wall.right
+
+    if on_ground_this_frame:
+        on_ground = True
+
         # Fire bullet
     if pygame.mouse.get_pressed()[0]:
         now = pygame.time.get_ticks()
@@ -88,6 +174,11 @@ while True:
         bullet[1] += bullet[3]
         if bullet[0] > WIDTH or bullet[0] < 0 or bullet[1] > HEIGHT or bullet[1] < 0:
             bullets.remove(bullet)
+        # Remove bullet if it hits a wall
+        bullet_rect = pygame.Rect(bullet[0], bullet[1], bullet_width, bullet_height)
+        for tile_type, wall in get_wall_rects():
+            if bullet_rect.colliderect(wall) and bullet in bullets:
+                bullets.remove(bullet)
 
 
     # Draw
@@ -95,6 +186,9 @@ while True:
 
     # Ground
     pygame.draw.rect(screen, WHITE, (0, ground_y, WIDTH, HEIGHT - ground_y))
+
+    # Map
+    draw_map(screen)
 
     # Square
     pygame.draw.rect(
